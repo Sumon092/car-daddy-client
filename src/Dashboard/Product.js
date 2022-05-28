6f8a5038bde1bd8e5b5a01c4554e79cfa0bac8ea
sumon092 <m.sumon@outlook.com>
2022-05-28 17:11:53 +0600

    product delete system added

--- src/Dashboard/Product.js
+++ src/Dashboard/Product.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const Product = ({ item }) => {
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/products/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount === 1) {
+                    toast.success(`delete successful`)
+                }
+            })
+    }
     return (
         <div class="card card-compact bg-base-100 shadow-xl">
             <figure><img className='w-60' src={item.img} alt="Shoes" /></figure>
@@ -11,7 +24,7 @@ const Product = ({ item }) => {
                 <p>Minimum Order: {item.minOrder}</p>
                 <p>Price: {item.price}</p>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-error text-white">Delete Product</button>
+                    <button onClick={() => handleDelete(item._id)} class="btn btn-error text-white">Delete Product</button>
                 </div>
             </div>
         </div>
